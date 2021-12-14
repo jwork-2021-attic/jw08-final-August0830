25dce4b55f497f7baba9a389866b80b6c4b211be
August0830 <mzl0830@sina.com>
2021-12-14 19:39:30 +0800

    input v1.0  finish

--- RebelliousMonster/RebelliousMonster-app/src/test/java/com/Augustus/app/LogTest.java
+++ RebelliousMonster/RebelliousMonster-app/src/test/java/com/Augustus/app/LogTest.java
@@ -1,21 +1,65 @@
 package com.Augustus.app;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
+
+import com.Augustus.app.com.anish.screen.ByteUtil;
 
 public class LogTest {
-    public static void main(String[] args) {
-        ArrayList<Thread> threads = new ArrayList<Thread>();
-        int n=1;
-        for (int i = 0; i < n; ++i) {
-            threads.add(new Thread(new Counter(i + 1)));
-            threads.get(i).start();
+    public static void main(String[] args) throws InterruptedException, IOException {
+        //ArrayList<Thread> threads = new ArrayList<Thread>();
+        input();
+        BufferedInputStream fis = new BufferedInputStream(new FileInputStream("./log.txt"));
+        Scanner sc = new Scanner(fis);
+        sc.useDelimiter("\n");
+        while(sc.hasNext()){
+
         }
+        sc.close();
     }
 
+    static void input() throws InterruptedException, IOException {
+        ArrayList<Counter> counters = new ArrayList<Counter>();
+        int n=3;
+        for (int i = 0; i < n; ++i) {
+            counters.add(new Counter(i+1));
+            // threads.add(new Thread(counters.get(i)));
+            counters.get(i).start();
+        }
+
+        BufferedOutputStream fos = new BufferedOutputStream(new FileOutputStream("./log.txt"));
+        byte[] logBuffer = ByteUtil.toByteArray(counters);
+        Long start = System.currentTimeMillis();
+        while(System.currentTimeMillis()-start<=1000*n){
+            fos.write(logBuffer);
+            fos.write(ByteUtil.toByteArray("\n"));
+            for(int i=0;i<n;++i)
+                counters.get(i).showVal();
+            logBuffer = ByteUtil.toByteArray(counters);
+            Thread.sleep(500);
+        }
+
+        for(int i=0;i<n;++i){
+            counters.get(i).interrupt();
+            System.out.println(i+1+" counter is stop:"+counters.get(i).isInterrupted());
+        }
+       
+        fos.close();
+    }
 }
 
-class Counter implements Runnable {
+class Counter extends Thread implements Serializable {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
     Random r;
     int val;
     int id;
@@ -38,16 +82,18 @@ class Counter implements Runnable {
     public void run() {
         // TODO Auto-generated method stub
         while (true) {
-            showVal();
+            if(this.isInterrupted())
+                return;
             val = r.nextInt(100);
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
+                break;
             }
         }
-
+        System.out.println(id+" stop running");
     }
 
 }
